25650faa6af4b39fd9f9bc1f62c4359c7bf9d758
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-07 19:13:14 +0530

    json package and edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./components/Header";
+import Feature from "./components/Feature.jsx";
+import Plans from "./components/Plans";
+import Footer from "./components/Footer";
+import ChooseInterview  from "./pages/ChooseInterview";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+   {/* <Header/>
+  <Feature/>
+  <Plans/>
+  <Footer/> */}
+<ChooseInterview/>
+  </div>;
 }
 
 export default App;
