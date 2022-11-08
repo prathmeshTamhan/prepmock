c28bce7240f13839cdbf5ecb8f3027fc6e9e6e0a
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-08 21:16:09 +0530

    Updated ChooseInterview page!

--- src/pages/ChooseInterview.jsx
+++ src/pages/ChooseInterview.jsx
@@ -1,55 +1,70 @@
-import React, { useState } from "react";
-
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
+import React from "react";
 import Navbar from "../components/Navbar";
+import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import "../pagescss/Domain.css";
+
 function ChooseInterview() {
   const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "light" ? "#fff" : "#231955",
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
     ...theme.typography.body2,
     padding: theme.spacing(1),
     textAlign: "center",
     color: theme.palette.text.secondary,
   }));
-  const style = {
-    section: {
-      color: "#231955",
-      backgroundColor: "#231955",
-      justifyContent: "space-around",
-      position: "relative",
-      padding: "200px 350px",
-    },
-  };
+
   return (
-    <div>
+    <div id="domain">
       <Navbar />
-      <div style={style.section}>
-        <Box>
-          <Grid container spacing={3} xs={0}>
-              <Item
-                sx={{
-                  height: 240,
-                  width: 300,
-                }}
-              >
-                <h1>TECHNICAL ROUND</h1>
-                <Button variant="contained">Lets Begin</Button>
-              </Item>
-              <Item
-                sx={{
-                  height: 240,
-                  width: 300,
-                }}
-              >
-                <h1>HR ROUND</h1>
-                <Button variant="contained">Lets Begin</Button>
-              </Item>
-          </Grid>
-        </Box>
-      </div>
+      <h1 className="heading">Choose Type of Interview for preparation</h1>
+      <Stack
+        direction="row"
+        divider={<Divider orientation="vertical" flexItem />}
+        spacing={3}
+      >
+        <Item
+          sx={{
+            height: 250,
+            width: 350,
+            fontSize: 34,
+            fontWeight: 15,
+            paddingTop: 4,
+
+            borderRadius: 4,
+            color: "RGB(31,70,144)",
+          }}
+        >
+          {"Technical Round"}
+          <h6 className="h6">
+            Technical Round of any interview include various questions from
+            different domains to check how technically sound interviwee is? This
+            section include questions from different technical domains
+          </h6>
+          <Button variant="contained">Go Ahead</Button>
+        </Item>
+        <Item
+          sx={{
+            height: 250,
+            width: 350,
+            fontSize: 33,
+            fontWeight: 13,
+            paddingTop: 4,
+            borderRadius: 4,
+            color: "RGB(31,70,144)",
+          }}
+        >
+          {"HR Round"}
+          <h6 className="h6">
+            HR round include general and habitual questions to check interviwee
+            personality traits and to check various behavioural factors to
+            determine interviwee's actual personality.
+          </h6>
+          <Button variant="contained">Go Ahead</Button>
+        </Item>
+      </Stack>
     </div>
   );
 }
