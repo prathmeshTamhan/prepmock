37196481122be74b669a85b9629b07247f684044
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-09 23:55:58 +0530

    Login And sign in added

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,23 @@ import Header from "./components/Header";
 import Feature from "./components/Feature.jsx";
 import Plans from "./components/Plans";
 import Footer from "./components/Footer";
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ChooseInterview  from "./pages/ChooseInterview";
 import ChooseDomain from "./pages/ChooseDomain";
 import Chooselevel from "./pages/Chooselevel"
+
 function App() {
   return <div>
    {/* <Header/>
   <Feature/>
   <Plans/>
   <Footer/> */}
-<ChooseInterview/>
+{/* <ChooseInterview/> */}
 {/* <ChooseDomain/> */}
 {/* <Chooselevel/> */}
-
+{/* <Login/> */}
+<Register/>
   </div>;
 }
 
