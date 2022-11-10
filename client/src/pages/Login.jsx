378a1cd6cc2a50edf1a41214db7b0da7f0f50992
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-10 13:28:04 +0530

    Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,56 +1,72 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import image from "../images/feature_1.png";
+import Button from "@mui/material/Button";
+import Link from '@mui/material/Link';
+import "../pagescss/signinlogin.css";
 function Login() {
-    const [email, setEmail] = useState("")
-	const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	async function loginUser(event) {
-		event.preventDefault()
+  async function loginUser(event) {
+    event.preventDefault();
 
-		const response = await fetch('http://localhost:1337/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		})
+    const response = await fetch("http://localhost:1337/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-		const data = await response.json()
+    const data = await response.json();
 
-		if (data.user) {
-			localStorage.setItem('token', data.user)
-			alert('Login successful')
-			window.location.href = '/dashboard'
-		} else {
-			alert('Please check your username and password')
-		}
-	}
+    if (data.user) {
+      localStorage.setItem("token", data.user);
+      alert("Login successful");
+      window.location.href = "/ChooseInterview";
+    } else {
+      alert("Please check your username and password");
+    }
+  }
 
   return (
-    <div>
+    <div className="container">
+		<Navbar/>
+		<div className="login-child">
+		<h1>Lets Begin</h1>
+		<h5>Start your Interview preparation journey with us!</h5>
+	<span>	<img src={image} id="image" ></img></span>
+		
+	  </div>
+      <div className="login-section">
         <h1>Login</h1>
-			<form onSubmit={loginUser}>
-				<input
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-					type="email"
-					placeholder="Email"
-				/>
-				<br />
-				<input
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-					type="password"
-					placeholder="Password"
-				/>
-				<br />
-				<input type="submit" value="Login" />
-			</form>
+        <form onSubmit={loginUser}>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Email"
+          />
+          <br />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Password"
+          />
+          <br />
+		  <Button variant="contained"  type="submit" value="Login" >Log in</Button>
+        </form>
+		<h6>Don't have an account?  <Link href="#" color="inherit">
+        {'Sign Up'}
+      </Link></h6>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
