c0f4cd2fe32e9b36c63f918e25b9c798f79f202d
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-13 15:42:23 +0530

    Added Hr section

--- server/models/que.model.js
+++ server/models/que.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const subjects = ['CN', 'DBMS', 'OS']
+const subjects = ['CN', 'DBMS', 'OS', 'HR']
 
 const CN = new mongoose.Schema(
     {
@@ -26,8 +26,16 @@ const OS = new mongoose.Schema(
     }
 )
 
+const HR= new   mongoose.Schema(
+    {
+        que: { type: String },
+        ansLink: { type: String },
+    }
+)
+
 const DBMSSchema = mongoose.model('DBMS', DBMS)
 const OSSchema = mongoose.model('OS', OS)
 const CNSchema = mongoose.model('CN', CN)
+const HRSchema= mongoose.model('HR',HR)
 
-module.exports = {OSSchema: OSSchema , DBMSSchema : DBMSSchema , CNSchema : CNSchema }
+module.exports = {OSSchema: OSSchema , DBMSSchema : DBMSSchema , CNSchema : CNSchema , HRSchema: HRSchema}
