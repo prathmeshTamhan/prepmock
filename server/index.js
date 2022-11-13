6121e3e5159c1f63c06f75a68e2f79a6e5f65899
prathmeshTamhan <prathmeshtamhan28@gmail.com>
2022-11-13 13:30:01 +0530

    NA

--- server/index.js
+++ server/index.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const User = require('./models/user.model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const dotenv = require("dotenv")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+const authRoute = require("./Routes/auth");
+const queRoute = require("./Routes/que");
 
-const authRoute = require("./Routes/auth")
-const queRoute = require("./Routes/que")
-
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-	await mongoose.connect(process.env.MONGO_URL).then(() => console.log("DB COnnection Succesful"));
+  await mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DB Connection Succesful"));
 }
 
-app.use("/auth" , authRoute)
-app.use("/que" , queRoute)
+app.use("/auth", authRoute);
+app.use("/que", queRoute);
 
 app.listen(1337, () => {
-	console.log('Server started on 1337')
-})
+  console.log("Server started on 1337");
+});
