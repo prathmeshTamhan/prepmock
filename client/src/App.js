4982c1e58b052c427e4f2cb216c87a7b24b21add
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2022-11-18 18:17:10 +0530

    Routing started

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,22 @@ import Plans from "./components/Plans";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ChooseInterview  from "./pages/ChooseInterview";
+import ChooseInterview from "./pages/ChooseInterview";
 import ChooseDomain from "./pages/ChooseDomain";
 import Chooselevel from "./pages/Chooselevel"
 import Start from "./pages/Start";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
-  return <div>
-   {/* <Header/>
-  <Feature/> */}
-  <Plans/>
-  {/* <Footer/> */}
-{/* <ChooseInterview/> */}
-{/* <ChooseDomain/> */}
-{/* <Chooselevel/> */}
-{/* <Login/> */}
-{/* <Register/> */}
-{/* <Start/> */}
-
-  </div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 export default App;
