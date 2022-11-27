d569002b132274075d67d395bfc8cc6f3167042e
bhargavi-005 <81408303+bhargavi-005@users.noreply.github.com>
2022-11-27 16:42:10 +0530

    app updated

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,31 @@ import ChooseDomain from './pages/ChooseDomain'
 import ChooseLevel from './pages/Chooselevel'
 import Instructions from './pages/Instruction'
 import StartTest from './pages/StartTest'
+import VidHomePage from "./components/HomePage/VidHomePage";
+import CallPage from "./components/CallPages/CallPage";
+import No_Match from "./components/NoMatch/No_Match";
 
 function App() {
   return (
     <>
         <Routes>
-          <Route exact path="/" element={<HomePage/>} />
+          {/* <Route exact path="/" element={<HomePage/>} />
           <Route exact path="/login" element={<Login/>} />
           <Route exact path="/register" element={ <Register/>} />
           <Route exact path="/chooseInterview" element={ <ChooseInterview/>} />
           <Route exact path="/chooseDomain" element={ <ChooseDomain/>} />
-          
           <Route exact path="/chooseLevel" element={ <ChooseLevel/>} />
           <Route exact path="/instructions" element={ <Instructions/>} />
-          <Route exact path="/Start" element={ <StartTest/>} />
+          <Route exact path="/Start" element={ <StartTest/>} /> */}
+          {/* <Route exact path="/VidHomePage" element={ <VidHomePage/>} /> */}
+          {/* <Route exact path="/CallPage" element={ <CallPage/>} />
+          <Route exact path="/No_Match" element={ <No_Match/>} /> */}
         </Routes>
+        {/* <VidHomePage/> */}
+        <CallPage/>
+        {/* <No_Match/> */}
     </>
+    
   )
 }
 
