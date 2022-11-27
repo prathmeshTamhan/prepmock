8d8b7437232ee1a721968595ec9bf30b50ac9b18
bhargavi-005 <81408303+bhargavi-005@users.noreply.github.com>
2022-11-27 16:43:01 +0530

    vid Home page updated

---
+++ client/src/components/HomePage/VidHomePage.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import  '../HomePage/vidHomepage.css';
+import VidHeader from '../UI/Header/VidHeader';
+import vectormeet from '../../images/vectormeet.jpg';
+
+import{
+  faVideo,
+  faKeyboard,
+}from "@fortawesome/free-solid-svg-icons"
+function VidHomePage() {
+  return (
+    
+    <div className='home-page'>
+      <VidHeader/>
+      <div className='body'>
+        <div className='left-side'>
+          <div className='content'>
+            <h1>Click below to join Meet</h1>
+            <div className='action-btn'>
+              <button className='btn green'>
+                <FontAwesomeIcon className="icon-block" icon ={faVideo}/>
+                New Meeting
+              </button>
+            
+            <div className='input-block'>
+              <div className='input-section'>
+                <FontAwesomeIcon className="icon-block" icon={faKeyboard}/>
+                <input placeholder='Enter a code or link'/>
+              </div>
+              <button className='btn no bg'>Join</button>
+            </div>
+          </div>
+          </div>
+        </div>
+        <div className='right-side'>
+          <div className='content'>
+            <img src={vectormeet} alt="vector image"/>
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
+  )
+}
+
+
+export default VidHomePage;
